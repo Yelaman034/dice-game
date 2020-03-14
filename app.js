1dbecad8eae7b78477ea20e542433af56b58385c
Yelaman <elamansky99@gmail.com>
2020-03-14 20:35:23 +0800

    dice-iig sanamsargui baidlaar orchuuldeg bogoson

--- app.js
+++ app.js
@@ -8,22 +8,36 @@ var scores = [0, 0];
 var roundScore = 0;
 
 //Шоо аль талаар буусныг хадгалах хувьсагч 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүй байдлаар үүснэ.
-
-var dice;
-dice = Math.random() * 6 + 1;
-dice = Math.floor(dice);
-
-console.log("Шоо " + dice);
+var diceNumber = Math.floor(Math.random() * 6 + 1);
 
 // window.document.querySelector("#score-0").textContent = dice;
 
 //Хоёр тоглогчийн оноог 0 болгий.
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
 
 //Шоо аль тал буусаныг алаг болгий
-document.querySelector(".dice").style.display = "none";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
 //Тоглогчийн roundScore 0 болгий.
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+//Roll dice btn идэвтэй болгох!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+//1.Callback function бичсэн нь
+
+// document.querySelector(".btn-roll").addEventListener("click", dicePlay);
+// function dicePlay() {
+//     var diceNumber;
+//     diceNumber = Math.random() * 6 + 1;
+//     diceNumber = Math.floor(diceNumber);
+//     alert("Шоо шидлээ: " + diceNumber);
+//   }
+//2.Anonymous function бичсэн зүгээр юм байна. Мэргэжлийн маяг
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  var diceNumber = Math.floor(Math.random() * 6 + 1);
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
